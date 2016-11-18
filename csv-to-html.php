6ec7c9fd55d7fd4bed7f5143340033057580d789
toskde177 <toskde177@outlook.com>
2016-11-18 06:07:22 -0600

    csv-to-html

---
+++ csv-to-html.php
@@ -0,0 +1,48 @@
+<?php
+$file = fopen("something.csv","r");
+
+$firstTimeHeader = true;
+$firstTimeBody = true;
+
+echo "<table>";
+while(! feof($file))
+{
+    $data = fgetcsv($file);
+    
+    if($firstTimeHeader)
+    {
+        echo "<thead>";
+    }
+    else
+    {
+        if($firstTimeBody)
+        {
+            echo "</thead>";
+            echo "<tbody>";
+            $firstTimeBody = false;
+        }
+    }
+    echo "<tr>";
+    
+    foreach ($data as $value)
+    {
+        if($firstTimeHeader)
+        {
+            echo "<th>" . $value . "</th>";
+        }
+        else
+        {
+            echo "<td>" . $value . "</td>";
+        }
+    }
+    
+    echo "</tr>";
+    if($firstTimeHeader)
+    {
+        $firstTimeHeader = false;
+    }
+}
+echo "</table>";
+
+fclose($file);
+?>
